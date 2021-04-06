8730716c739af11a80209ea73b74e1ecb6d1ad6c
Lucas Galeano <lucas.galeano@estudiantes.utec.edu.uy>
2021-04-06 01:08:12 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express")
+const morgan = require("morgan");
+
+const app = express()
+
+app.use(express.json());
+
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log(req.body);
+});
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params);
+});
+
+app.put("/api/v1/restaurants/:id", (req, res) =>{
+    console.log(req.params.id);
+    console.log(req.body);
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`)
+})
