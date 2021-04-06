8d8643654882433f3e25ce7cb3b50cdedeed52ed
Lucas Galeano <lucas.galeano@estudiantes.utec.edu.uy>
2021-04-06 11:33:00 -0300

    Day 2

--- server/server.js
+++ server/server.js
@@ -1,18 +1,43 @@
 require("dotenv").config();
-
 const express = require("express")
 const morgan = require("morgan");
+
+const db = require('./db')
 
 const app = express()
 
 app.use(express.json());
 
-app.get("/api/v1/restaurants", (req, res) => {
-    console.log(req.body);
+//Get all restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+    try{
+        const results = await db.query("SELECT * FROM restaurants");
+        console.log(results);   
+        res.status(200).json({
+            status:"success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows
+            }
+        })
+    }catch(err){
+        console.log(err);
+    }
 });
 
-app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params);
+//Get restaurant from ID
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+    try{
+        const results = await db.query(`SELECT * FROM restaurants where id = ${req.params.id}`);
+        res.status(200).json({
+            status:"success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    }catch(err){
+        console.log(err)
+    }
 });
 
 app.put("/api/v1/restaurants/:id", (req, res) =>{
